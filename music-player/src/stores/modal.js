5316335881841f163cf23ae6e08f50c068fcc5c6
mehmethalis <mehmethaliscicek@gmail.com>
2023-02-04 14:18:29 +0300

    state magement pinia

---
+++ music-player/src/stores/modal.js
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export default defineStore("modal", {
+  state: () => ({ isOpen: false }),
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? "hidden" : "";
+    },
+  },
+});
